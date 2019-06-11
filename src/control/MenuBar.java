75b475fe70a2b42705ff88aa19afb5b392dff319
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-06-11 19:06:26 +0200

    Info Msg

--- src/control/MenuBar.java
+++ src/control/MenuBar.java
@@ -22,6 +22,23 @@ public class MenuBar extends MenuBarGui {
     }
 
     private void setActions() {
+        String info = "Este es un programa desarrollado en Java\n por\n" +
+                "estudiantes del Centro de Formacion Profesional\n" +
+                " Daniel Castelao de Vigo para el ciclo de D.A.M 1,\n" +
+                " Modulo de Programacion.\n" +
+                "\n" +
+                "Este software esta pensado para facilitar la tarea\n" +
+                " de organizacion y control del servicio de un negocio\n" +
+                " de hosteleria. \n" +
+                "\n" +
+                "Version 0.0.1\n" +
+                "\n" +
+                "Autores. Francisco Sanchez y Emilio Mendez\n" +
+                "\n" +
+                "Agradecimientos especiales a Nina Fontaiña\n" +
+                " Profesora del Modulo y Tutora del Curso.\n";
+
+
         jMenuItemCargar.addActionListener(e -> setCard(LOADPANEL));
         jMenuItemNueva.addActionListener(e -> setCard(NEWPANEL));
 
@@ -36,6 +53,7 @@ public class MenuBar extends MenuBarGui {
             PanelPrincipal.clearMemory();
             JOptionPane.showMessageDialog(SwingUtilities.getWindowAncestor(this), "Cache eliminado", "Limpiando", JOptionPane.INFORMATION_MESSAGE);
         });
+        jMenuItemInformacion.addActionListener(e -> JOptionPane.showMessageDialog(this, info, "Informacion", JOptionPane.INFORMATION_MESSAGE));
     }
 
 }
